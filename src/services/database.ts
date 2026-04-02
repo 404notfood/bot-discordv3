ab9d203bb53ee7eeb5811463fe09a715b4bf0a84
Laurent <l.prado56920@gmail.com>
2026-04-02 08:39:20 +0200

    fix prisma

--- src/services/database.ts
+++ src/services/database.ts
@@ -1,4 +1,5 @@
 import { PrismaClient } from '../generated/prisma/client';
+import { PrismaPg } from '@prisma/adapter-pg';
 import { log } from './logger';
 
 /**
@@ -10,7 +11,8 @@ class DatabaseManager {
   private isConnected = false;
 
   private constructor() {
-    this.prisma = new PrismaClient();
+    const adapter = new PrismaPg(process.env.DATABASE_URL!);
+    this.prisma = new PrismaClient({ adapter });
   }
 
   /**
